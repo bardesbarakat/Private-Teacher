54ccf9f2b9f0a6eb73a2d1432a9dd4aed5126f6b
Bardes Barakat <bardesbarakat@hotmail.com>
2026-08-08 23:18:39 +0300

    change

--- backend/BEdu.API/obj/Debug/net8.0/BEdu.API.AssemblyInfo.cs
+++ backend/BEdu.API/obj/Debug/net8.0/BEdu.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BEdu.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe04ad3d4dd152d1532035bef47cf92ccd92a2e9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ceb633075309eabef3bc1b6edae45ccd03592259")]
 [assembly: System.Reflection.AssemblyProductAttribute("BEdu.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BEdu.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
