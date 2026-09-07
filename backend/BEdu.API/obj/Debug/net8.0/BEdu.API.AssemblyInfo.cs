2b7807777f41a4a45fce6b6532b5e1c3dfd33281
Bardes Barakat <bardesbarakat@hotmail.com>
2026-09-07 22:29:28 +0300

    feat: update navigation brand styles for improved layout and visibility

--- backend/BEdu.API/obj/Debug/net8.0/BEdu.API.AssemblyInfo.cs
+++ backend/BEdu.API/obj/Debug/net8.0/BEdu.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BEdu.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1269402f834b6e068dde39ce119c9393163c1e75")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9fa2010202d88bcacf86aeb4f3e902e1933bcfe4")]
 [assembly: System.Reflection.AssemblyProductAttribute("BEdu.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BEdu.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
